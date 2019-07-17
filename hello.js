6d4b1fa3d8e0326827e0fd2a00f534378cd40c96
Omar De Leo <omar.deleo@gmail.com>
2019-07-17 19:39:53 -0400

    Initial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+const port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis and Heroku!\n');
+}).listen(port, '0.0.0.0');
+
+console.log(`Server running at http://0.0.0.0:${port}/`);
